1aca2db9e7ff92ae89fdb1ea8a713278f845e5fd
AlejandroMesaX <mesa.10.alejandro@gmail.com>
2025-12-05 00:14:55 -0500

    Creacion entidad Pedido

--- domicilios/domicilios/src/main/java/com/gofast/domicilios/domain/repository/PedidoRepositoryPort.java
+++ domicilios/domicilios/src/main/java/com/gofast/domicilios/domain/repository/PedidoRepositoryPort.java
@@ -1,12 +1,19 @@
 package com.gofast.domicilios.domain.repository;
 
 import com.gofast.domicilios.domain.model.Pedido;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface PedidoRepositoryPort {
     Pedido save(Pedido pedido);
+
     Optional<Pedido> findById(Long id);
+
     List<Pedido> findByClienteId(Long clienteId);
+
     List<Pedido> findByDomiciliarioId(Long domiciliarioId);
+
+    List<Pedido> findAll();
+
 }
