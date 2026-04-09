bc9a107330bbd7d27281e22ea43ef166b83aa3d1
AlejandroMesaX <mesa.10.alejandro@gmail.com>
2026-04-08 20:31:59 -0500

    Despliegue V2

--- domicilios/domicilios/src/main/java/com/gofast/domicilios/infrastructure/config/CorsConfig.java
+++ domicilios/domicilios/src/main/java/com/gofast/domicilios/infrastructure/config/CorsConfig.java
@@ -12,7 +12,10 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:5174"));
+        config.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "http://localhost:5174",
+                "https://*.vercel.app"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 
