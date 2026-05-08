f142b95e6f7f16316d101f52c4768b8f7e519d42
AlejandroMesaX <mesa.10.alejandro@gmail.com>
2026-05-07 21:48:52 -0500

    Deployment 1

--- domicilios/domicilios/src/main/java/com/gofast/domicilios/infrastructure/config/CorsConfig.java
+++ domicilios/domicilios/src/main/java/com/gofast/domicilios/infrastructure/config/CorsConfig.java
@@ -7,27 +7,44 @@ import java.util.List;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of(
-                "http://localhost:5173",
-                "http://localhost:5174",
-                "https://*.vercel.app",
-                "https://frontend-i6v4rmwhy-alejandromesaxs-projects.vercel.app/"));
-
+        config.setAllowedOriginPatterns(List.of("*")); // temporal para probar
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-
-        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-
+        config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("Authorization"));
-
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
         return source;
     }
 }
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration config = new CorsConfiguration();
+//
+//        config.setAllowedOriginPatterns(List.of(
+//                "http://localhost:5173",
+//                "http://localhost:5174",
+//                "https://*.vercel.app",
+//                "https://frontend-i6v4rmwhy-alejandromesaxs-projects.vercel.app/"));
+//
+//        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+//
+//        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+//
+//        config.setExposedHeaders(List.of("Authorization"));
+//
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
+//}
